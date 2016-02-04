a5c761a9c284867927904fde5e23aa05eaeee8f3
Sam Drew <sam@outlook.com>
2016-02-04 16:12:59 +0000

    Added a .gitignore, and modified the program to run indefintely, at hard coded time intervals.

--- speed_test_tweeter/speed_test_tweet.py
+++ speed_test_tweeter/speed_test_tweet.py
@@ -1,12 +1,23 @@
 from speedtest import *
 from tweet import *
+import time
 
-accessToken = input("Enter the accessToken: ")
-accessTokenSecret = input("Enter the accessTokenSecret: ")
-consumerSecret = input("Enter the consumerSecret: ")
-consumerKey = input("Enter the consumerKey: ")
-speed = getSpeed("all")
+def testTweet():
+    if speed[1] < 15.00:
+        print(speed[1])
+        tweet = ("Hey, my ISP, why is my internet speed " + str(speed[1]) + "mb/s, when I pay for 50mb/s??")
+        writeTweet(accessToken, accessTokenSecret, consumerSecret, consumerKey, tweet)
 
-if speed[1] < 15.0:
-    tweet = ("Hey, my ISP, why is my internet speed " + speed[1] + " when I pay for 50mb/s??")
-    writeTweet(accessToken, accessTokenSecret, consumerSecret, consumerKey)
+if __name__ == "__main__":
+    accessToken = input("Enter the accessToken: ")
+    accessTokenSecret = input("Enter the accessTokenSecret: ")
+    consumerSecret = input("Enter the consumerSecret: ")
+    consumerKey = input("Enter the consumerKey: ")
+    speed = getSpeed("all")
+    run = True
+    counter = 1
+    while run == True:
+        testTweet()
+        print(counter)
+        counter += 1
+        time.sleep(60)
